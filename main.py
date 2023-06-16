d03594cec255cee5aa73779eb3be28ee14faf494
z-ovc <zzz_oveysi@yahoo.com>
2023-06-16 23:03:04 +0330

    send mail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 #make a request
 api_key = "ee08e398f6784a11a79b9f35bd11ed20"
@@ -9,5 +10,10 @@ request = requests.get(url)
 content = request.json()
 
 #access the articles
-for article in content["articls"]:
-    print(content["articles"])
+body=""
+for article in content["articles"]:
+    if article["title"] and article["description"] is not None:
+        body = body+article["title"]+article["description"]
+
+body = body.encode("utf-8")
+send_email(body)
---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "zazaovc44@gmail.com"
+    password = "llwpaqturxdpxvzj"
+
+    receiver = "statistiker74@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
