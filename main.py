8baf194548fd3389f4fcc52475f61276c4e295ed
Zahra Oveisi <z.oveisi@ebcom.ir>
2023-06-17 10:14:07 +0330

    add subject and dynamic topic

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from send_email import send_email
 
 #make a request
 api_key = "ee08e398f6784a11a79b9f35bd11ed20"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-05-16&sortBy=publishedAt&apiKey=ee08e398f6784a11a79b9f35bd11ed20"
+topic = "tesla"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-05-17&sortBy=publishedAt&apiKey=ee08e398f6784a11a79b9f35bd11ed20"
 request = requests.get(url)
 
 #get a fictionary of data
@@ -13,7 +14,7 @@ content = request.json()
 body=""
 for article in content["articles"]:
     if article["title"] and article["description"] is not None:
-        body = body+article["title"]+article["description"]
+        body = "Subject: today-news\n" +body+article["title"]+article["description"]+"\n"+article["url"]+2*"\n"
 
 body = body.encode("utf-8")
 send_email(body)
